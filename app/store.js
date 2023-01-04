36966bfb50e684dbe908ff9f57871e87168fae55
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-05 01:37:00 +0600

    dynamic Slice reducer

--- app/store.js
+++ app/store.js
@@ -1,9 +1,10 @@
 const { configureStore } = require("@reduxjs/toolkit");
 
 const counterReducer = require("../feature/counter/counterSlice");
+const dynamicCounterReducer = require("../feature/dynamicCounterSlice/dynamicCounterSlice");
 
 const store = configureStore({
-  reducer: { counter: counterReducer },
+  reducer: { counter: counterReducer, dynamicCounter: dynamicCounterReducer },
 });
 
 module.exports = store;
