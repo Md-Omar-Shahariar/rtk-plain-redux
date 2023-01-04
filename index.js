8c66b6a16ea9227ce11d1d88154ded54791a4a74
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-05 01:38:08 +0600

    dynamic counter done

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const store = require("./app/store");
 const { counterAction } = require("./feature/counter/counterSlice");
+const {
+  dynamicActions,
+} = require("./feature/dynamicCounterSlice/dynamicCounterSlice");
 
 store.subscribe(() => {
   console.log(store.getState());
 });
 
 store.dispatch(counterAction.increment());
+store.dispatch(dynamicActions.increment(4));
