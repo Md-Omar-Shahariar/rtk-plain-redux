70acf658ce4afa4361411df58a3b21226d35eada
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-05 01:10:53 +0600

    clean

--- index.js
+++
@@ -1,8 +0,0 @@
-const store = require("./app/store");
-const { counterActions } = require("./feature/counter/counterSlice");
-
-store.subscribe(() => {
-  console.log(store.getState());
-});
-
-store.dispatch(counterActions.increment());
