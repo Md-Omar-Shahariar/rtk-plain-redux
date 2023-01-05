0d6c461b70d04e38079ef4a8392d819e3a309720
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-05 12:26:54 +0600

    Middle Ware

--- app/store.js
+++ app/store.js
@@ -1,10 +1,14 @@
 const { configureStore } = require("@reduxjs/toolkit");
+const { createLogger } = require("redux-logger");
+
+const logger = createLogger();
 
 const counterReducer = require("../feature/counter/counterSlice");
 const dynamicCounterReducer = require("../feature/dynamicCounterSlice/dynamicCounterSlice");
 
 const store = configureStore({
   reducer: { counter: counterReducer, dynamicCounter: dynamicCounterReducer },
+  middleware: (getDefaultMiddlewares) => getDefaultMiddlewares().concat(logger),
 });
 
 module.exports = store;
