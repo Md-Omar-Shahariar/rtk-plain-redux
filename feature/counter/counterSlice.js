756ad57b181ddefe94144d666956705db994552b
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-05 14:48:08 +0600

    extra reducer

--- feature/counter/counterSlice.js
+++ feature/counter/counterSlice.js
@@ -1,4 +1,7 @@
 const { createSlice } = require("@reduxjs/toolkit");
+const {
+  dynamicActions,
+} = require("../dynamicCounterSlice/dynamicCounterSlice");
 
 const initialState = {
   value: 0,
@@ -14,6 +17,11 @@ const counterSlice = createSlice({
     decrement: (state) => {
       state.value--;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(dynamicActions.increment, (state, action) => {
+      state += action.payload;
+    });
   },
 });
 
